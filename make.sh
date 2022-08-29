dad8206ae9a263004aee94bbe1c5ba59c1200c05
Thomas J. Fan <thomasjpfan@gmail.com>
2022-08-29 17:05:09 -0400

    MNT Update diff

--- make.sh
+++ make.sh
@@ -6,11 +6,10 @@ popd
 
 PATCH_FILE=patch.diff
 
-diff -u hdbscan/hdbscan/_hdbscan_boruvka.pyx scikit-learn/sklearn/cluster/_hdbscan/_hdbscan_boruvka.pyx > $PATCH_FILE
-diff -u hdbscan/hdbscan/_hdbscan_linkage.pyx scikit-learn/sklearn/cluster/_hdbscan/_hdbscan_linkage.pyx  >> $PATCH_FILE
-diff -u hdbscan/hdbscan/_hdbscan_reachability.pyx scikit-learn/sklearn/cluster/_hdbscan/_hdbscan_reachability.pyx >> $PATCH_FILE
-diff -u hdbscan/hdbscan/_hdbscan_tree.pyx scikit-learn/sklearn/cluster/_hdbscan/_hdbscan_tree.pyx >> $PATCH_FILE
-diff -u hdbscan/hdbscan/hdbscan_.py scikit-learn/sklearn/cluster/_hdbscan/hdbscan_.py >> $PATCH_FILE
+diff -u hdbscan/hdbscan/_hdbscan_linkage.pyx scikit-learn/sklearn/cluster/_hdbscan/_linkage.pyx  >> $PATCH_FILE
+diff -u hdbscan/hdbscan/_hdbscan_reachability.pyx scikit-learn/sklearn/cluster/_hdbscan/_reachability.pyx >> $PATCH_FILE
+diff -u hdbscan/hdbscan/_hdbscan_tree.pyx scikit-learn/sklearn/cluster/_hdbscan/_tree.pyx >> $PATCH_FILE
+diff -u hdbscan/hdbscan/hdbscan_.py scikit-learn/sklearn/cluster/_hdbscan/hdbscan.py >> $PATCH_FILE
 
 diff2html -i file -F index_raw.html -t "HDBSCAN PR Diff" -- $PATCH_FILE
 
